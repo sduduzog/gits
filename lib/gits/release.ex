d21e0482be0def0d4ee322cf65c364130014fa4b
Sdu <bs.gumede@outlook.com>
2024-05-14 02:52:59 +0200

    reload migrator

--- lib/gits/release.ex
+++ lib/gits/release.ex
@@ -1,16 +1,16 @@
 defmodule Gits.Release do
-  @moduledoc """
-  Used for executing DB release tasks when run in production without Mix
-  installed.
-  """
   @app :gits
-
   def migrate do
     load_app()
 
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
+  end
+
+  def migrate_all do
+    load_app()
+    migrate()
   end
 
   def rollback(repo, version) do
