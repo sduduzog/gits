8b0841eef93cc8494bd5d32ec9fd7937aef424bc
Sdu <bs.gumede@outlook.com>
2024-05-21 01:53:54 +0200

    create new layout

--- lib/gits_web/controllers/user_controller.ex
+++ lib/gits_web/controllers/user_controller.ex
@@ -2,6 +2,8 @@ defmodule GitsWeb.UserController do
   use GitsWeb, :controller
 
   def events(conn, _) do
-    conn |> render(:events)
+    conn
+    |> put_layout({GitsWeb.Layouts, :next})
+    |> render(:events)
   end
 end
