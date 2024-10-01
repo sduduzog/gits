bcb8b5c2afb3d8ff9d38b747de71abe5ce43adbe
Sdu <bs.gumede@outlook.com>
2024-10-01 18:33:54 +0200

    homepage pizaz

--- lib/gits_web/live/host_live/manage_event.ex
+++ lib/gits_web/live/host_live/manage_event.ex
@@ -84,7 +84,7 @@ defmodule GitsWeb.HostLive.ManageEvent do
     """
   end
 
-  def render(%{live_action: :edit, step: :feature_graphics} = assigns) do
+  def render(%{live_action: :edit, step: :feature_graphic} = assigns) do
     ~H"""
     <.wizard_wrapper step={@step} subtitle="The Utlimate Cheese Festival">
       <div class="grid grid-cols-2 gap-4 pt-4">
@@ -126,7 +126,28 @@ defmodule GitsWeb.HostLive.ManageEvent do
   def render(%{live_action: :edit, step: :tickets} = assigns) do
     ~H"""
     <.wizard_wrapper step={@step} subtitle="The Utlimate Cheese Festival">
-      <div class="grid grid-cols-2 gap-4">
+      <div class="grid grid-cols-2 gap-4 pt-4">
+        <div class="col-span-full grid place-items-center gap-2">
+          <svg xmlns="http://www.w3.org/2000/svg" class="size-20 text-zinc-400" viewBox="0 0 48 48">
+            <path
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M37.962 14.925a3.456 3.456 0 0 1-4.887-4.887L28.539 5.5L5.5 28.538l4.538 4.538a3.456 3.456 0 0 1 4.887 4.886l4.537 4.538L42.5 19.462Zm-16.056-2.793L24 14.226m1.862 1.862l2.094 2.094m1.862 1.862l2.094 2.094M33.774 24l2.094 2.094"
+            />
+          </svg>
+          <span class="text-sm font-medium text-zinc-900">No tickets</span>
+          <span class="text-sm text-zinc-500">
+            Get started by creating adding a new ticket
+          </span>
+        </div>
+        <div class="col-span-full flex justify-center py-8">
+          <button class="inline-flex items-center justify-center gap-2 rounded-lg border px-4 py-2">
+            <.icon name="hero-plus-mini" />
+            <span class="text-sm font-medium">Add Ticket</span>
+          </button>
+        </div>
         <div class="col-span-full flex justify-end gap-6">
           <button class="rounded-lg px-4 py-2 text-zinc-950 hover:bg-zinc-100" phx-click="back">
             <span class="text-sm font-medium">Back</span>
@@ -143,7 +164,56 @@ defmodule GitsWeb.HostLive.ManageEvent do
   def render(%{live_action: :edit, step: :payment_method} = assigns) do
     ~H"""
     <.wizard_wrapper step={@step} subtitle="The Utlimate Cheese Festival">
-      <div class="grid grid-cols-2 gap-4">
+      <div class="grid grid-cols-2 gap-8 pt-4">
+        <fieldset :if={false} class="col-span-full grid gap-4 lg:grid-cols-2">
+          <legend class="col-span-full inline-flex text-sm font-medium">
+            Choose a payment method
+          </legend>
+
+          <label class="mt-1 flex gap-2 rounded-lg border p-4 has-[:checked]:ring-2 has-[:checked]:ring-zinc-600">
+            <input type="radio" name="event-payment-method" checked class="peer sr-only" />
+            <div class="grid grow gap-2">
+              <span class="text-sm font-medium text-zinc-950">Paystack</span>
+              <span class="text-sm text-zinc-500">
+                Automated payouts. Limited ticket sale volumes
+              </span>
+            </div>
+            <.icon
+              name="hero-check-circle-mini"
+              class="shrink-0 opacity-0 peer-checked:opacity-100 text-zinc-700"
+            />
+          </label>
+
+          <label class="mt-1 flex gap-2 rounded-lg border p-4 has-[:checked]:ring-2 has-[:checked]:ring-zinc-600 has-[:disabled]:opacity-60">
+            <input type="radio" name="event-payment-method" class="peer sr-only" />
+            <div class="grid grow gap-2">
+              <span class="text-sm font-medium text-zinc-950">Payfast</span>
+              <span class="text-sm text-zinc-500">
+                Requires you to create a merchant account on
+                <.link class="text-zinc-950" href="https://payfast.co.za" target="_blank">
+                  payfast.co.za
+                </.link>
+              </span>
+            </div>
+            <.icon
+              name="hero-check-circle-mini"
+              class="opacity-0 shrink-0 peer-checked:opacity-100 text-zinc-700"
+            />
+          </label>
+        </fieldset>
+
+        <div class="col-span-full grid gap-4 rounded-2xl border border-zinc-300 p-4">
+          <div class="inline-flex items-start gap-4">
+            <.icon name="hero-information-circle-mini" class="text-blue-500 shrink-0" />
+            <span class="font-semibold">Tickets are free. We're skipping this step</span>
+          </div>
+          <div class="space-y-2">
+            <p class="text-sm text-zinc-800">
+              When all the tickets for the event are free, you don't need to setup a payment method for the event. You can skip this step for now.
+            </p>
+          </div>
+        </div>
+
         <div class="col-span-full flex justify-end gap-6">
           <button class="rounded-lg px-4 py-2 text-zinc-950 hover:bg-zinc-100" phx-click="back">
             <span class="text-sm font-medium">Back</span>
@@ -160,13 +230,143 @@ defmodule GitsWeb.HostLive.ManageEvent do
   def render(%{live_action: :edit, step: :summary} = assigns) do
     ~H"""
     <.wizard_wrapper step={@step} subtitle="The Utlimate Cheese Festival">
+      <div class="pt-4">
+        <dl class="grid gap-6">
+          <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt class="text-sm font-medium leading-6 text-gray-900">Event name</dt>
+            <dd class="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              <span class="flex-grow">The Ultimate Cheese Festival</span>
+              <span class="ml-4 flex-shrink-0">
+                <button
+                  type="button"
+                  class="rounded-md bg-white font-medium text-zinc-600 hover:text-zinc-500"
+                >
+                  Update
+                </button>
+              </span>
+            </dd>
+          </div>
+
+          <div class="sm:grid sm:grid-cols-3 sm:gap-4">
+            <dt class="text-sm font-medium leading-6 text-gray-900">Start date</dt>
+            <dd class="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              <span class="flex-grow">24 October 2024, 11:45 PM</span>
+            </dd>
+          </div>
+
+          <div class=" sm:grid sm:grid-cols-3 sm:gap-4 ">
+            <dt class="text-sm font-medium leading-6 text-gray-900">End date</dt>
+            <dd class="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              <span class="flex-grow">24 October 2024, 11:45 PM</span>
+            </dd>
+          </div>
+
+          <div class="sm:grid sm:grid-cols-3 sm:gap-4">
+            <dt class="text-sm font-medium leading-6 text-gray-900">Description</dt>
+            <dd class="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              <span class="flex-grow">
+                Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
+              </span>
+            </dd>
+          </div>
+
+          <div class=" sm:grid sm:grid-cols-3 sm:gap-4 ">
+            <dt class="text-sm font-medium leading-6 text-gray-900">Location</dt>
+            <dd class="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              <span class="flex-grow">Artistry JHB</span>
+              <span class="ml-4 flex-shrink-0">
+                <button
+                  type="button"
+                  class="rounded-md bg-white font-medium text-zinc-600 hover:text-zinc-500"
+                >
+                  Update
+                </button>
+              </span>
+            </dd>
+          </div>
+
+          <div class=" sm:grid sm:grid-cols-3 sm:gap-4 ">
+            <dt class="text-sm font-medium leading-6 text-gray-900">Feature Graphic</dt>
+            <dd class="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              <div class="grow">
+                <div class="aspect-[3/2] h-28 bg-zinc-100"></div>
+              </div>
+              <span class="ml-4 flex-shrink-0">
+                <button
+                  type="button"
+                  class="rounded-md bg-white font-medium text-zinc-600 hover:text-zinc-500"
+                >
+                  Update
+                </button>
+              </span>
+            </dd>
+          </div>
+
+          <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt class="text-sm font-medium leading-6 text-gray-900">Tickets</dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              <ul role="list" class="divide-y divide-gray-100 rounded-md border border-gray-200">
+                <li class="flex items-center justify-between py-4 pr-5 pl-4 text-sm leading-6">
+                  <div class="flex w-0 flex-1 items-center">
+                    <.icon name="hero-ticket" class="size-5 shrink-0" />
+                    <div class="ml-4 flex min-w-0 flex-1 gap-2">
+                      <span class="grow truncate font-medium">Early Access</span>
+                      <span class="flex-shrink-0 text-gray-400">R 80.00</span>
+                    </div>
+                  </div>
+                  <div class="ml-4 flex flex-shrink-0 space-x-4">
+                    <button
+                      type="button"
+                      class="rounded-md bg-white font-medium text-zinc-600 hover:text-zinc-500"
+                    >
+                      Update
+                    </button>
+                  </div>
+                </li>
+                <li class="flex items-center justify-between py-4 pr-5 pl-4 text-sm leading-6">
+                  <div class="flex w-0 flex-1 items-center">
+                    <.icon name="hero-ticket" class="size-5 shrink-0" />
+                    <div class="ml-4 flex min-w-0 flex-1 gap-2">
+                      <span class="grow truncate font-medium">General</span>
+                      <span class="flex-shrink-0 text-gray-400">R 100.00</span>
+                    </div>
+                  </div>
+                  <div class="ml-4 flex flex-shrink-0 space-x-4">
+                    <button
+                      type="button"
+                      class="rounded-md bg-white font-medium text-zinc-600 hover:text-zinc-500"
+                    >
+                      Update
+                    </button>
+                  </div>
+                </li>
+              </ul>
+            </dd>
+          </div>
+
+          <div class=" sm:grid sm:grid-cols-3 sm:gap-4 ">
+            <dt class="text-sm font-medium leading-6 text-gray-900">Payment Method</dt>
+            <dd class="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              <span class="flex-grow">None</span>
+              <span class="ml-4 flex-shrink-0">
+                <button
+                  type="button"
+                  class="rounded-md bg-white font-medium text-zinc-600 hover:text-zinc-500"
+                >
+                  Update
+                </button>
+              </span>
+            </dd>
+          </div>
+        </dl>
+      </div>
       <div class="grid grid-cols-2 gap-4">
         <div class="col-span-full flex justify-end gap-6">
           <button class="rounded-lg px-4 py-2 text-zinc-950 hover:bg-zinc-100" phx-click="back">
             <span class="text-sm font-medium">Back</span>
           </button>
           <button class="rounded-lg bg-zinc-900 px-4 py-2 text-zinc-50" phx-click="continue">
-            <span class="text-sm font-medium">Continue</span>
+            <span class="text-sm font-medium">Publish Event</span>
           </button>
         </div>
       </div>
@@ -180,11 +380,6 @@ defmodule GitsWeb.HostLive.ManageEvent do
       <div class="grid grid-cols-2 gap-8 pt-4">
         <label for="" class="col-span-full grid gap-1">
           <span class="text-sm font-medium">Name</span>
-          <input type="text" class="w-full rounded-lg border-zinc-300 px-3 py-2 text-sm" />
-        </label>
-
-        <label for="" class="col-span-full grid gap-1">
-          <span class="text-sm font-medium">Venue</span>
           <input type="text" class="w-full rounded-lg border-zinc-300 px-3 py-2 text-sm" />
         </label>
 
@@ -219,8 +414,6 @@ defmodule GitsWeb.HostLive.ManageEvent do
   end
 
   def handle_params(%{"edit" => current_destination}, _uri, socket) do
-    current_destination |> IO.inspect(label: "fooooooooooooooooo")
-
     socket =
       case current_destination do
         "details" ->
@@ -229,8 +422,8 @@ defmodule GitsWeb.HostLive.ManageEvent do
         "location" ->
           socket |> assign(:step, :location)
 
-        "graphics" ->
-          socket |> assign(:step, :feature_graphics)
+        "graphic" ->
+          socket |> assign(:step, :feature_graphic)
 
         "tickets" ->
           socket |> assign(:step, :tickets)
@@ -264,8 +457,8 @@ defmodule GitsWeb.HostLive.ManageEvent do
     next_step =
       case socket.assigns.step do
         :location -> "details"
-        :feature_graphics -> "location"
-        :tickets -> "graphics"
+        :feature_graphic -> "location"
+        :tickets -> "graphic"
         :payment_method -> "tickets"
         :summary -> "payments"
       end
@@ -279,8 +472,8 @@ defmodule GitsWeb.HostLive.ManageEvent do
     next_step =
       case socket.assigns.step do
         :event_details -> "location"
-        :location -> "graphics"
-        :feature_graphics -> "tickets"
+        :location -> "graphic"
+        :feature_graphic -> "tickets"
         :tickets -> "payments"
         :payment_method -> "summary"
         :summary -> "summary"
--- lib/gits_web/live/host_live/manage_event_components.ex
+++ lib/gits_web/live/host_live/manage_event_components.ex
@@ -2,8 +2,7 @@ defmodule GitsWeb.HostLive.ManageEventComponents do
   use Phoenix.Component
   use GitsWeb, :verified_routes
 
-  alias Phoenix.LiveView.JS
-  import GitsWeb.Gettext
+  # alias Phoenix.LiveView.JS
 
   attr :current, :boolean, required: true
   attr :label, :string, required: true
@@ -32,7 +31,7 @@ defmodule GitsWeb.HostLive.ManageEventComponents do
       case assigns.step do
         :location -> "Location"
         :event_details -> "Event Details"
-        :feature_graphics -> "Feature Graphics"
+        :feature_graphic -> "Feature Graphic"
         :tickets -> "Tickets"
         :payment_method -> "Payment Method"
         :summary -> "Summary"
@@ -45,7 +44,7 @@ defmodule GitsWeb.HostLive.ManageEventComponents do
       <div class="flex justify-between gap-5 py-4 lg:grid lg:gap-8 lg:pt-36">
         <.wizard_step current={@step == :event_details} label="Event Details" />
         <.wizard_step current={@step == :location} label="Location" />
-        <.wizard_step current={@step == :feature_graphics} label="Feature Graphics" />
+        <.wizard_step current={@step == :feature_graphic} label="Feature Graphic" />
         <.wizard_step current={@step == :tickets} label="Tickets" />
         <.wizard_step current={@step == :payment_method} label="Payment Method" />
         <.wizard_step current={@step == :summary} label="Summary" />
