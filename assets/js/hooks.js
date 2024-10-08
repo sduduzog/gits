92d704d5700d5ed681b71fcdfb9b14d4be52e494
Sdu <bs.gumede@outlook.com>
2024-10-08 04:34:36 +0200

    remove commented out code

--- assets/js/hooks.js
+++ assets/js/hooks.js
@@ -43,18 +43,6 @@ const QrScannerCameraList = {
 };
 
 const QrScanner = {
-  // initialiseCamera() {
-  //   const { id, label } = this.currentCamera;
-  //   const span = this.el.querySelector("span#camera-label");
-  //   span.innerText = label;
-  //   this.html5QrCode = new Html5Qrcode("scanner", {
-  //     formatsToSupport: [Html5QrcodeSupportedFormats.QR_CODE],
-  //   });
-  //   this.html5QrCode.start(id, scanConfig, async (decodedText) => {
-  //     await this.html5QrCode.stop();
-  //     this.pushEvent("scanned", decodedText);
-  //   });
-  // },
   async mounted() {
     const cameraId = this.el.dataset.camera;
     const html5QrCode = new Html5Qrcode(this.el.id, {
@@ -69,23 +57,6 @@ const QrScanner = {
     html5QrCode.start(cameraId, scanConfig, async (decodedText) => {
       console.log({ decodedText });
     });
-    // const rotateCamera = this.el.querySelector("button#rotate-camera");
-    // rotateCamera.addEventListener("click", async () => {
-    //   const index = this.cameras.findIndex(
-    //     (camera) => camera.id === this.currentCamera.id,
-    //   );
-    //   const nextIndex = index === this.cameras.length - 1 ? 0 : index + 1;
-    //   this.currentCamera = this.cameras[nextIndex];
-    //   window.localStorage.setItem("camera_id", this.currentCamera.id);
-    //   await this.html5QrCode.stop();
-    //   this.initialiseCamera();
-    // });
-    // this.cameras = await Html5Qrcode.getCameras();
-    // const id = window.localStorage.getItem("camera_id");
-    // const previousCamera = this.cameras.find((camera) => camera.id === id);
-    // const [camera] = this.cameras;
-    // this.currentCamera = previousCamera || camera;
-    // this.initialiseCamera();
   },
 };
 
