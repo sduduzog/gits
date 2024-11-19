01118cfbe22687d7cff68643e4960f5d0e29b9c0
Sdu <gumedesduduzo@gmail.com>
2024-11-19 19:27:07 +0200

    feat: update email rendering and add order confirmation
    - change `render` in `gitsweb.emailcontroller` to use
      `:order_confirmation` template.
    - add new `order_confirmation.html.heex` for order details.
    - update confirmation message in `event_order.html.heex`
      to reflect successful order placement.

--- lib/gits_web/controllers/email_controller.ex
+++ lib/gits_web/controllers/email_controller.ex
@@ -6,6 +6,6 @@ defmodule GitsWeb.EmailController do
     |> put_root_layout(false)
     |> put_layout(false)
     |> assign(:token, "foo")
-    |> render(:magic_link)
+    |> render(:order_confirmation)
   end
 end
