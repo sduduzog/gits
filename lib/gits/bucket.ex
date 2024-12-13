037410f5a4bb6f0b49d1f25b16a7a4bd0e3b58fc
Sdu <gumedesduduzo@gmail.com>
2024-12-13 17:37:58 +0200

    feat: add placeholder image url handling and visibility option
    - introduce `get_image_url/1` function to return a placeholder
      image when `nil` is passed.
    - update visibility options in the event details template to
      only include `:private`, enhancing clarity for users.

--- lib/gits/bucket.ex
+++ lib/gits/bucket.ex
@@ -1,10 +1,15 @@
 defmodule Gits.Bucket do
+  def get_image_url(nil) do
+    "/images/placeholder.png"
+  end
+
   def get_image_url(filename) do
     presigned_url_options = Application.get_env(:gits, :presigned_url_options)
 
     bucket_name = Application.get_env(:gits, :bucket_name)
 
-    with {:ok, _} <- ExAws.S3.head_object(bucket_name, filename) |> ExAws.request(),
+    with {:ok, _} <-
+           ExAws.S3.head_object(bucket_name, filename) |> ExAws.request(),
          {:ok, signed_url} <-
            ExAws.Config.new(:s3)
            |> ExAws.S3.presigned_url(:get, bucket_name, filename, presigned_url_options) do
