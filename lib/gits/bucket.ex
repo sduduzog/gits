7edd1463d387cade355c65c1a70ca4c5539ce855
Sdu <gumedesduduzo@gmail.com>
2025-01-11 15:37:24 +0200

    feat: update image url handling and layout components
    - change `gits.bucket.get_image_url/1` to return `{:ok, url}`
      for better error handling.
    - introduce `gits.bucket.get_image_url!/1` for direct url access.
    - update layout to include `settings` in the alias.
    - adjust html templates to use the new `get_image_url!/1` function.
    - improve layout styling for better user experience.

--- lib/gits/bucket.ex
+++ lib/gits/bucket.ex
@@ -1,22 +1,31 @@
 defmodule Gits.Bucket do
   def get_image_url(nil) do
-    "/images/placeholder.png"
+    {:ok, "/images/placeholder.png"}
   end
 
   def get_image_url(filename) do
     presigned_url_options = Application.get_env(:gits, :presigned_url_options)
-
     bucket_name = Application.get_env(:gits, :bucket_name)
 
-    with {:ok, _} <-
-           ExAws.S3.head_object(bucket_name, filename) |> ExAws.request(),
-         {:ok, signed_url} <-
-           ExAws.Config.new(:s3)
-           |> ExAws.S3.presigned_url(:get, bucket_name, filename, presigned_url_options) do
-      signed_url
-    else
-      _ ->
-        "/images/placeholder.png"
+    Cachex.fetch(:cache, filename, fn key ->
+      with {:ok, _} <-
+             ExAws.S3.head_object(bucket_name, key) |> ExAws.request(),
+           {:ok, signed_url} <-
+             ExAws.Config.new(:s3)
+             |> ExAws.S3.presigned_url(:get, bucket_name, key, presigned_url_options) do
+        {:ok, signed_url}
+      else
+        _ ->
+          {:ignore, "/images/placeholder.png"}
+      end
+    end)
+    |> case do
+      {_, url} -> url
     end
+  end
+
+  def get_image_url!(filename) do
+    {:ok, url} = get_image_url(filename)
+    url
   end
 end
